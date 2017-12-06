4f644eee52e24f2a7dc30186141cf7f65ac82fdc
Toni Hoffmann <toni-hoffmann@xremix.de>
2017-12-06 09:40:34 +0100

    build package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/header/header.component';
-// import { HeaderModule } from './modules/header/header.module';
+import { HeaderModule } from './modules/header/header.module';
 
 
 @NgModule({
